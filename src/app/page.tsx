7ac3ecded7f729c2de02ef9edee3d0661fb07bf6
“mrahxd” <sahebabdullah7860@gmail.com>
2025-08-16 18:07:36 +0530

    changed profile image from png to jpeg due to large size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="flex justify-center">
           <Image
-            src="/profile.png"
+            src="/profile.jpeg"
             alt="A picture of Abdullah Mohd"
             width={300}
             height={300}
